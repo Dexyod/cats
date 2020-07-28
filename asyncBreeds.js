261a28c3eae47829b5b5127b030e8498a3e026b4
Dexyod <dexyod43@gmail.com>
2020-07-28 19:23:02 +0000

    added testing

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -2,18 +2,15 @@ const fs = require("fs");
 
 const breedDetailsFromFile = function (breed, callback) {
   console.log("breedDetailsFromFile: Calling readFile...");
-  fs.readFile(`cats/data/${breed}.txt`, "utf8", (error, data) => {
+
+  fs.readFile(`./data/${breed}.txt`, "utf8", (error, data) => {
     console.log("In readFile's Callback: it has the data.");
     if (error) {
-      console.log(error.message);
+      callback(undefined);
     } else {
       callback(data);
     }
   });
 };
 
-const printData = (data) => {
-  console.log("Return Value: ", data);
-};
-
-breedDetailsFromFile("Bombay", printData);
+module.exports = breedDetailsFromFile;
--- syncBreeds.js
+++ syncBreeds.js
@@ -4,6 +4,7 @@ const catBreeds = {
     "Balinese are curious, outgoing, intelligent cats with excellent communication skills. They are known for their chatty personalities and are always eager to tell you their views on life, love, and what you’ve served them for dinner.",
   Bombay:
     "The golden eyes and the shiny black coat of the Bombay is absolutely striking. Likely to bond most with one family member, the Bombay will follow you from room to room and will almost always have something to say about what you are doing, loving attention and to be carried around, often on his caregiver's shoulder.",
+  Black: "",
 };
 
 // synchronous function to fetch a cat breed
@@ -12,6 +13,4 @@ const breedDetails = function (breed) {
   return catBreeds[breed];
 };
 
-// get the return value right away from the function
-const bombay = breedDetails("Bombay");
-console.log(bombay); //=> prints out the description for that bree
+module.exports = breedDetails;
